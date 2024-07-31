0f36b6dd2a6b0145db416b8c23ce533415584e36
Valeentin87 <valeentin87@list.ru>
2024-07-31 11:14:41 +0300

    реализован запрос на получение всех пользователей

---
+++ index.js
@@ -0,0 +1,19 @@
+// реализация API для управления пользователями
+// 1. Роут получения всех пользователей Название роута GET/users
+
+// Результат - массив пользователей
+
+const express = require('express');
+
+const app = express();
+
+const users = [];
+
+app.get('/', function(req, res) {
+    console.log('получен запрос на получение всех пользователей');
+    res.send({users});
+});
+
+app.listen(3000, () => {
+    console.log("Сервер запущен на порту 3000");
+});
