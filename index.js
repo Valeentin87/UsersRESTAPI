d60378008ce41f4617183870693ddae41c852453
Valeentin87 <valeentin87@list.ru>
2024-07-31 18:01:55 +0300

    Реализован роут изменения данных пользователя

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const express = require('express');
 
 const app = express();
 
+
 const users = [];
 let uniqueID = 0;  // уникальный номер пользователя
 
@@ -24,15 +25,20 @@ app.get('/users', function(req, res) {
 });
 
 // 2. Роут создания позьзователя
+
+app.use(express.json());
+
 app.post('/users', (req, res) => {
     uniqueID += 1;
     users.push( {
         id: uniqueID,
         ...req.body
     });
-    console.log('получен запрос на создание нового пользователя');
+    console.log('получен запрос на создание нового пользователя', req.body);
+    
     res.send({
-        id: uniqueID
+        id: uniqueID,
+        ...req.body
     });
 });
 
@@ -43,15 +49,43 @@ app.get('/users/:id', (req, res) => {
 
     if (user) {
         console.log("Пользователь найден!!!");
-        res.send({ user});
+        res.send({ user });
     }
     else {
         console.log("Пользователь не найден!!!");
         res.status(404);
         res.send({ user: null });
     }
-})
+});
+
+//4. Роут обновления данных о пользователе
+
+//app.use(express.json);
+
+app.put('/users/:id', (req, res) => {
+    const user = users.find((user) => user.id === Number(req.params.id));
+
+    if (user) {
+        console.log(user);
+        console.log(user.firstName);
+        user.firstName = req.body.firstName;
+        console.log(user.secondName);
+        user.secondName = req.body.secondName;
+        user.age = req.body.age;
+        user.city = req.body.city;
+        
+        console.log("Данные пользователя изменены ");
+        res.send({ user });
+    }
+    else {
+        console.log('Произошла ошибка! ');
+        res.status(404);
+        res.send({user:null});
+    }
+});
 
 app.listen(3000, () => {
     console.log("Сервер запущен на порту 3000");
 });
+
+
